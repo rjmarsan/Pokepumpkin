2d54e9a18e1decf59c22db90c7b8d144bdfb3fc8
RJ Marsan <me@rj.io>
2012-11-02 23:08:10 -0500

    Successfully moved ports

--- pokedetector/pokedetector.ino
+++ pokedetector/pokedetector.ino
@@ -1,3 +1,7 @@
+#define TOUT 8
+#define TIN 9
+#define TGUARD 10
+
 int  i;
 unsigned int x, y;
 float accum, fout, fval = .07;    // these are variables for a simple low-pass (smoothing) filter - fval of 1 = no filter - .001 = max filter
@@ -8,10 +12,10 @@ void setup() {
   DDRB=B101;     // DDR is the pin direction register - governs inputs and outputs- 1's are outputs
   // Arduino pin 8 output, pin 9 input, pin 10 output for "guard pin"
   //  preceding line is equivalent to three lines below
-  //  pinMode(8, OUTPUT);     // output pin
-  //  pinMode(9, INPUT);      // input pin
-  //  pinMode(10, OUTPUT);    // guard pin
-  digitalWrite(10, LOW);  //could also be HIGH - don't use this pin for changing output though
+  pinMode(TOUT, OUTPUT);     // output pin
+  pinMode(TIN, INPUT);      // input pin
+  pinMode(TGUARD, OUTPUT);    // guard pin
+  digitalWrite(TGUARD, LOW);  //could also be HIGH - don't use this pin for changing output though
 }
 
 void loop() {
@@ -21,21 +25,21 @@ void loop() {
   for (i=0; i < 4 ; i++ ){       // do it four times to build up an average - not really neccessary but takes out some jitter
 
     // LOW-to-HIGH transition
-    PORTB = PORTB | 1;                    // Same as line below -  shows programmer chops but doesn't really buy any more speed
-    // digitalWrite(8, HIGH);    
-    // output pin is PortB0 (Arduino , sensor pin is PortB1 (Arduinio 9)                                   
+    //PORTD = PORTD | 1;                    // Same as line below -  shows programmer chops but doesn't really buy any more speed
+    digitalWrite(TOUT, HIGH);    
+    // output pin is PortD2 (Arduino , sensor pin is PortD3 (Arduinio )                                   
 
-    while ((PINB & B10) != B10 ) {        // while the sense pin is not high
-      //  while (digitalRead(9) != 1)     // same as above port manipulation above - only 20 times slower!                
+    //while ((PIND2 & D5) != D5 ) {        // while the sense pin is not high
+    while (digitalRead(TIN) != 1) {    // same as above port manipulation above - only 20 times slower!                
       x++;
     }
     delay(1);
 
     //  HIGH-to-LOW transition
-    PORTB = PORTB & 0xFE;                // Same as line below - these shows programmer chops but doesn't really buy any more speed
-    //digitalWrite(8, LOW);              
-    while((PINB & B10) != 0 ){            // while pin is not low  -- same as below only 20 times faster
-      // while(digitalRead(9) != 0 )      // same as above port manipulation - only 20 times slower!
+    //PORTD = PORTD & 0xFE;                // Same as line below - these shows programmer chops but doesn't really buy any more speed
+    digitalWrite(TOUT, LOW);              
+    //while((PIND2 & D5) != 0 ){            // while pin is not low  -- same as below only 20 times faster
+    while(digitalRead(TIN) != 0 ) {    // same as above port manipulation - only 20 times slower!
       y++;  
     }
 
